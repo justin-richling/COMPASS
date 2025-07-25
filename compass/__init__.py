5a28efa1ec46e1fdd6b2c20d6d5be8a10c4ed802
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-07-25 13:49:31 -0600

    Update __init__.py

--- compass/__init__.py
+++ compass/__init__.py
@@ -3,5 +3,13 @@ from .core.colormap_loader import register_colormaps
 register_colormaps()
 
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 print(plt.colormaps())
+
+fig = plt.figure(figsize=(8, 3))
+ax1 = fig.add_axes([0.05, 0.80, 0.9, 0.15])
+mpl.colorbar.ColorbarBase(ax1, cmap="viirs_ir_default",
+                                    orientation='horizontal')
+plt.show()
+
