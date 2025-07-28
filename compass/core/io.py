cbaa0f318990ce59df5b23cd3ea356d882608082
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-07-28 17:18:57 -0600

    Update io.py

--- compass/core/io.py
+++ compass/core/io.py
@@ -14,7 +14,7 @@ def get_cam_ds(usernl_path,cam_path):
     fincl_lat = indv_ahh[2].replace(":","_to_")
     print(fincl_lon,"   ",fincl_lat)
     
-    
+    hist_dict = {}
     #cam_path = Path(f"/glade/derecho/scratch/richling/cases/{case_name}/run/")
     h1a_files = sorted(Path(cam_path).glob("*h1a*00.nc"))
     h2i_files = sorted(Path(cam_path).glob("*h2i*00.nc"))
@@ -22,31 +22,42 @@ def get_cam_ds(usernl_path,cam_path):
     h0a_files = sorted(Path(cam_path).glob("*h0a*00.nc"))
     #nudge-socrates.cam.h0a_new_all.nc
     
-    h2i_ds = xr.open_mfdataset(h2i_files, combine="nested", concat_dim="time")
-    h2i_ds = h2i_ds.sortby('time')
+    if h2i_files:
+        h2i_ds = xr.open_mfdataset(h2i_files, combine="nested", concat_dim="time")
+        h2i_ds = h2i_ds.sortby('time')
+    else:
+        h2i_ds = None
+
+    if h1a_files:
+        h1a_ds = xr.open_mfdataset(h1a_files, combine="nested", concat_dim="time")
+        h1a_ds = h1a_ds.sortby('time')
+    else:
+        h1a_ds = None
     
-    h1a_ds = xr.open_mfdataset(h1a_files, combine="nested", concat_dim="time")
-    h1a_ds = h1a_ds.sortby('time')
+    if h0a_files:
+        h0a_ds = xr.open_mfdataset(h0a_files, combine="nested", concat_dim="time")
+        h0a_ds = h0a_ds.sortby('time')
+
+        # Rename the variables in the second dataset by removing the substring
+        new_var_names = {var: var.replace(f"_{fincl_latlon_str}", "") for var in h0a_ds.data_vars}
+        
+        # Apply the renaming to the dataset
+        h0a_ds = h0a_ds.rename(new_var_names)
+        
+        # Rename the variables in the second dataset by removing the substring
+        new_dim_names = {var: var.replace(f"_physgrid_{fincl_lon}", "") for var in h0a_ds.dims}
+        # Apply the renaming to the dataset
+        h0a_ds = h0a_ds.rename(new_dim_names)
+        
+        # Rename the variables in the second dataset by removing the substring
+        new_dim_names = {var: var.replace(f"_physgrid_{fincl_lat}", "") for var in h0a_ds.dims}
+        # Apply the renaming to the dataset
+        h0a_ds = h0a_ds.rename(new_dim_names)
+
+    else:
+        h0a_ds = None
     
-    h0a_ds = xr.open_mfdataset(h0a_files, combine="nested", concat_dim="time")
-    h0a_ds = h0a_ds.sortby('time')
-    h0a_ds
     
-    # Rename the variables in the second dataset by removing the substring
-    new_var_names = {var: var.replace(f"_{fincl_latlon_str}", "") for var in h0a_ds.data_vars}
-    
-    # Apply the renaming to the dataset
-    h0a_ds = h0a_ds.rename(new_var_names)
-    
-    # Rename the variables in the second dataset by removing the substring
-    new_dim_names = {var: var.replace(f"_physgrid_{fincl_lon}", "") for var in h0a_ds.dims}
-    # Apply the renaming to the dataset
-    h0a_ds = h0a_ds.rename(new_dim_names)
-    
-    # Rename the variables in the second dataset by removing the substring
-    new_dim_names = {var: var.replace(f"_physgrid_{fincl_lat}", "") for var in h0a_ds.dims}
-    # Apply the renaming to the dataset
-    h0a_ds = h0a_ds.rename(new_dim_names)
     
     print("All done")
     return h0a_ds, h1a_ds, h2i_ds
