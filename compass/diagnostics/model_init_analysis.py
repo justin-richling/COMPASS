e50eabe97417cfa1a6fc492534766109d72b14f1
justin-richling <56696811+justin-richling@users.noreply.github.com>
2025-07-28 17:02:17 -0600

    Update model_init_analysis.py

--- compass/diagnostics/model_init_analysis.py
+++ compass/diagnostics/model_init_analysis.py
@@ -11,7 +11,7 @@ from matplotlib.patches import Rectangle
 def main():
     init_casenl_name = "nudged-socrates-inithist-004-window"
     init_casenl_path = f"/glade/derecho/scratch/richling/{init_casenl_name}/user_nl_cam"
-    cam_path = Path(f"/glade/derecho/scratch/richling/{init_casenl_name}/run/")
+    init_casenl_path = Path(f"/glade/derecho/scratch/richling/{init_casenl_name}/run/")
     h0a_init_ds, h1a_init_ds, h2i_init_ds = get_cam_ds(init_casenl_path)
 
     case_name = "F2000climo.f09_f09_mg17.window.exp.6hrInit.R13.003" #exp_casenames[3]
